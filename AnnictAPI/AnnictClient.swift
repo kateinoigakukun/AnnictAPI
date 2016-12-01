7ec7832f8c0f1ab7d3afcf7fdf86e9a39815b9bd
Saito Yuta <kateinoigakukun@gmail.com>
2016-12-01 18:21:39 +0900

    Create AnnictClient

--- AnnictAPI/AnnictClient.swift
+++ AnnictAPI/AnnictClient.swift
@@ -2,8 +2,18 @@
 //  AnnictClient.swift
 //  AnnictAPI
 //
-//  Created by SaitoYuta on 2016/11/24.
+//  Created by SaitoYuta on 2016/12/01.
 //  Copyright © 2016年 kateinoigakukun. All rights reserved.
 //
 
 import Foundation
+import APIKit
+import Result
+
+class AnnictClient: Session {
+    init(accessToken: String) {
+        let configuration = URLSessionConfiguration.default
+        let adapter = AnnictSessionAdapter(accessToken: accessToken, configuration: configuration)
+        super.init(adapter: adapter)
+    }
+}
