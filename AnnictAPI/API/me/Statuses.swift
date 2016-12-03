b52efcf803891e67b712982c802c9c802915eab0
Saito Yuta <kateinoigakukun@gmail.com>
2016-12-03 19:02:00 +0900

    Create me/Statuses

---
+++ AnnictAPI/API/me/Statuses.swift
@@ -0,0 +1,55 @@
+//
+//  Statuses.swift
+//  AnnictAPI
+//
+//  Created by SaitoYuta on 2016/12/03.
+//  Copyright © 2016年 kateinoigakukun. All rights reserved.
+//
+
+import Foundation
+import APIKit
+
+class AnnictMeStatuses: AnnictRequest {
+
+    typealias Response = Void
+
+    var path: String {
+        return "/v1/me/statuses"
+    }
+    var method: HTTPMethod {
+        return .post
+    }
+
+    var queryParameters: [String : Any]? {
+        var parameters:[String : String] = [:]
+        parameters["work_id"] = work_id.description
+        parameters["kind"] = kind.description
+
+        return parameters
+    }
+
+    enum StatusKind: CustomStringConvertible {
+        case wanna_watch
+        case watching
+        case watched
+        case on_hold
+        case stop_watching
+        case no_select
+
+        var description: String {
+            return String(describing: self)
+        }
+    }
+
+    var work_id: Int
+    var kind: StatusKind
+
+    init(work_id: Int, kind: StatusKind) {
+        self.work_id = work_id
+        self.kind = kind
+    }
+
+    func response(from object: Any, urlResponse: HTTPURLResponse) throws -> () {
+        return
+    }
+}
